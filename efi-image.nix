1327632141bdbc9120069c2cd0983e165208c0c3
dramforever <dramforever@live.com>
2023-01-12 21:50:48 +0800

    Use NixOS options for resizing filesystem

--- efi-image.nix
+++ efi-image.nix
@@ -2,6 +2,8 @@
 
 {
   fileSystems.${config.boot.loader.efi.efiSysMountPoint}.neededForBoot = true;
+  fileSystems."/".autoResize = true;
+  boot.growPartition = true;
 
   system.build.efiImage =
     let
@@ -57,17 +59,8 @@
     if [ -f /nix-path-registration ]; then
       set -euo pipefail
       set -x
-      # Figure out device names for the boot device and root filesystem.
-      rootPart=$(${pkgs.util-linux}/bin/findmnt -n -o SOURCE /)
-      bootDevice=$(lsblk -npo PKNAME $rootPart)
-      partNum=$(lsblk -npo MAJ:MIN $rootPart | ${pkgs.gawk}/bin/awk -F: '{print $2}')
-      # Resize the root partition and the filesystem to fit the disk
-      echo ",+," | sfdisk -N$partNum --no-reread $bootDevice
-      ${pkgs.parted}/bin/partprobe
-      ${pkgs.e2fsprogs}/bin/resize2fs $rootPart
       # Register the contents of the initial Nix store
       ${config.nix.package.out}/bin/nix-store --load-db < /nix-path-registration
-      # nixos-rebuild also requires a "system" profile and an /etc/NIXOS tag.
       touch /etc/NIXOS
       ${config.nix.package.out}/bin/nix-env -p /nix/var/nix/profiles/system --set /run/current-system
       NIXOS_INSTALL_BOOTLOADER=1 /nix/var/nix/profiles/system/bin/switch-to-configuration boot
